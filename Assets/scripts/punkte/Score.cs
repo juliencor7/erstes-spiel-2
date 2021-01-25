a785aa9b1d114d48d87079bbcc48407d80bcca3b
juliencor7 <juliencordes7@gmail.com>
2021-01-25 13:50:03 +0100

    first init

---
+++ Assets/scripts/punkte/Score.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+   public int score;
+    public static Score instance;
+
+    void Start()
+    {
+
+        instance = this;
+
+
+    }
+    
+
+  
+    void Update()
+    {
+        
+
+
+
+    }
+
+    public void PunkteAddieren(int scoreAdd)
+    {
+
+        score += scoreAdd;
+
+        
+
+    }
+
+
+
+    
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/scripts/schiessen/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject hitEffect;
+    public int damage;
+    
+    
+  void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+       Enemy enemy = hitInfo.GetComponent<Enemy>();
+        
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+       
+        
+        Destroy(gameObject);
+
+        
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
